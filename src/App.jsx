e4696135a565b516ec18fd0e63aed9dc9d1e42db
idriss-bk <drissboukdirbk@gmail.com>
2024-11-05 20:26:59 +0400

    add button

--- src/App.jsx
+++ src/App.jsx
@@ -5,20 +5,25 @@ const products = [
   {id:2, title:"Product 2"},
   {id:3, title:"Product 3"},
 ];
-let list = [];
-for (const product of products) {
-list.push(<li >{product.title}</li>)
-}
+// let list = [];
+// for (const product of products) {
+// list.push(<li >{product.title}</li>)
+// }
 function App() {
+  const handleClick = () => {
+    console.log("Clicked!")
+  }
 
   return (
     <>
     <div>
-      <ul>
-        <li>{list}</li>
-
+      {/* <ul>
+        <li key={products.id}>{list}</li>
       </ul>
+      <br/>
+      {products.map(product => <li key={product.id}>{product.title}</li>)} */}
     </div>
+    <button onClick={handleClick}>Click</button>
     </>
   )
 }
